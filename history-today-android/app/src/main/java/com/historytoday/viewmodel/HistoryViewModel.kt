007b8755269db6076263bae0f319edc2f0ff47a0
hbtslcx-cc <hbtslcx@gmail.com>
2026-06-06 13:00:04 +0800

    fix: add missing import kotlinx.coroutines.flow.update in all ViewModels

--- history-today-android/app/src/main/java/com/historytoday/viewmodel/HistoryViewModel.kt
+++ history-today-android/app/src/main/java/com/historytoday/viewmodel/HistoryViewModel.kt
@@ -10,6 +10,7 @@ import com.historytoday.domain.usecase.GetEventsByDateUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.time.LocalDate
 import javax.inject.Inject
